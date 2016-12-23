ac2264a9aa24add962d499710d8aa3c29cac3d05
Mike Walch <mwalch@apache.org>
2016-12-23 14:39:41 -0500

    Added REST API to webserver

--- modules/ui/src/main/java/webindex/ui/WebServer.java
+++ modules/ui/src/main/java/webindex/ui/WebServer.java
@@ -21,6 +21,7 @@ import java.nio.file.Path;
 import java.util.Collections;
 import java.util.Optional;
 
+import com.google.gson.Gson;
 import freemarker.template.Configuration;
 import org.apache.accumulo.core.client.Connector;
 import org.apache.fluo.api.config.FluoConfiguration;
@@ -28,6 +29,7 @@ import org.apache.fluo.core.util.AccumuloUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import spark.ModelAndView;
+import spark.Request;
 import spark.Spark;
 import spark.template.freemarker.FreeMarkerEngine;
 import webindex.core.IndexClient;
@@ -38,24 +40,63 @@ import webindex.core.models.Pages;
 import webindex.core.models.TopResults;
 
 import static spark.Spark.get;
+import static spark.Spark.halt;
 import static spark.Spark.staticFiles;
 
 public class WebServer {
 
   private static final Logger log = LoggerFactory.getLogger(WebServer.class);
 
-  private static final ModelAndView VIEW_404 = new ModelAndView(null, "404.ftl");
+  private IndexClient client;
 
   public WebServer() {}
 
+  private TopResults getTop(Request req) {
+    String next = Optional.ofNullable(req.queryParams("next")).orElse("");
+    Integer pageNum = Integer.parseInt(Optional.ofNullable(req.queryParams("pageNum")).orElse("0"));
+    return client.getTopResults(next, pageNum);
+  }
+
+  private Page getPage(Request req) {
+    String url = req.queryParams("url");
+    if (url == null) {
+      halt(400, "Bad request: url parameter was not set");
+    }
+    return client.getPage(url);
+  }
+
+  private Pages getPages(Request req) {
+    String domain = req.queryParams("domain");
+    if (domain == null) {
+      halt(400, "Bad request: domain parameter was not set");
+    }
+    String next = Optional.ofNullable(req.queryParams("next")).orElse("");
+    Integer pageNum = Integer.parseInt(Optional.ofNullable(req.queryParams("pageNum")).orElse("0"));
+    return client.getPages(domain, next, pageNum);
+  }
+
+  private Links getLinks(Request req) {
+    String rawUrl = req.queryParams("url");
+    if (rawUrl == null) {
+      halt(400, "Bad request: url parameter was not set");
+    }
+    String linkType = req.queryParams("linkType");
+    if (linkType == null) {
+      halt(400, "Bad request: linkType parameter was not set");
+    }
+    String next = Optional.ofNullable(req.queryParams("next")).orElse("");
+    Integer pageNum = Integer.parseInt(Optional.ofNullable(req.queryParams("pageNum")).orElse("0"));
+    return client.getLinks(rawUrl, linkType, next, pageNum);
+  }
+
   public void start(IndexClient client, int port, Path templatePath) {
+    this.client = client;
 
     Spark.port(port);
 
     staticFiles.location("/assets");
 
     FreeMarkerEngine freeMarkerEngine = new FreeMarkerEngine();
-
     if (templatePath != null && Files.exists(templatePath)) {
       log.info("Serving freemarker templates from {}", templatePath.toAbsolutePath());
       Configuration freeMarkerConfig = new Configuration();
@@ -69,50 +110,23 @@ public class WebServer {
 
     get("/", (req, res) -> new ModelAndView(null, "home.ftl"), freeMarkerEngine);
 
-    get("/top",
-        (req, res) -> {
-          String next = Optional.ofNullable(req.queryParams("next")).orElse("");
-          Integer pageNum =
-              Integer.parseInt(Optional.ofNullable(req.queryParams("pageNum")).orElse("0"));
-          TopResults results = client.getTopResults(next, pageNum);
-          return new ModelAndView(Collections.singletonMap("top", results), "top.ftl");
-        }, freeMarkerEngine);
+    get("/top", (req, res) -> new ModelAndView(Collections.singletonMap("top", getTop(req)),
+        "top.ftl"), freeMarkerEngine);
 
-    get("/page", (req, res) -> {
-      if (req.queryParams("url") == null) {
-        return VIEW_404;
-      }
-      String url = req.queryParams("url");
-      Page page = client.getPage(url);
-      return new ModelAndView(Collections.singletonMap("page", page), "page.ftl");
-    }, freeMarkerEngine);
+    Gson gson = new Gson();
+    get("/api/top", (req, res) -> getTop(req), gson::toJson);
 
-    get("/pages",
-        (req, res) -> {
-          if (req.queryParams("domain") == null) {
-            return VIEW_404;
-          }
-          String domain = req.queryParams("domain");
-          String next = Optional.ofNullable(req.queryParams("next")).orElse("");
-          Integer pageNum =
-              Integer.parseInt(Optional.ofNullable(req.queryParams("pageNum")).orElse("0"));
-          Pages pages = client.getPages(domain, next, pageNum);
-          return new ModelAndView(Collections.singletonMap("pages", pages), "pages.ftl");
-        }, freeMarkerEngine);
+    get("/page", (req, res) -> new ModelAndView(Collections.singletonMap("page", getPage(req)),
+        "page.ftl"), freeMarkerEngine);
+    get("/api/page", (req, res) -> getPage(req), gson::toJson);
 
-    get("/links",
-        (req, res) -> {
-          if (req.queryParams("url") == null || req.queryParams("linkType") == null) {
-            return VIEW_404;
-          }
-          String rawUrl = req.queryParams("url");
-          String linkType = req.queryParams("linkType");
-          String next = Optional.ofNullable(req.queryParams("next")).orElse("");
-          Integer pageNum =
-              Integer.parseInt(Optional.ofNullable(req.queryParams("pageNum")).orElse("0"));
-          Links links = client.getLinks(rawUrl, linkType, next, pageNum);
-          return new ModelAndView(Collections.singletonMap("links", links), "links.ftl");
-        }, freeMarkerEngine);
+    get("/pages", (req, res) -> new ModelAndView(Collections.singletonMap("pages", getPages(req)),
+        "pages.ftl"), freeMarkerEngine);
+    get("/api/pages", (req, res) -> getPages(req), gson::toJson);
+
+    get("/links", (req, res) -> new ModelAndView(Collections.singletonMap("links", getLinks(req)),
+        "links.ftl"), freeMarkerEngine);
+    get("/api/links", (req, res) -> getLinks(req), gson::toJson);
   }
 
   public void stop() {
